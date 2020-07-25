2c909a148fd8847ffff4979cc622a1ee38235df5
Luis Ponce Contreras <40501992+LuchoPonceC@users.noreply.github.com>
2020-07-25 10:27:43 -0500

    Antes de Malograrlo TODO

--- app/src/main/java/com/example/meowtecfinal/Nosotros.java
+++ app/src/main/java/com/example/meowtecfinal/Nosotros.java
@@ -6,8 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Nosotros extends AppCompatActivity {
 
@@ -15,13 +30,6 @@ public class Nosotros extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nosotros);
-
-
-
-        
-
-
-
 
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -59,6 +67,53 @@ public class Nosotros extends AppCompatActivity {
 
     }
 
+    public void enviar(View view){
+        String url = "http://52.67.175.153:8080/contacto";
+        String nombre = ((EditText)findViewById(R.id.editTextTextPersonName1)).getText().toString();
+        String apellido = ((EditText)findViewById(R.id.editTextTextPersonName2)).getText().toString();
+        String celular = ((EditText)findViewById(R.id.editTextPhone)).getText().toString();
+        String correo = ((EditText)findViewById(R.id.editTextTextEmailAddress2)).getText().toString();
+        String mensaje = ((EditText)findViewById(R.id.editTextTextMultiLine)).getText().toString();
+        Map<String, String> message = new HashMap<String, String>();
+        message.put("nombre", nombre);
+        message.put("apellidos", apellido);
+        message.put("celular", celular);
+        message.put("correo", correo);
+        message.put("mensaje", mensaje);
 
+        JSONObject jsonMessage = new JSONObject(message);
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                jsonMessage,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        showMessage("Enviado con exito");
+                        goMainActivity();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }
+        );
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+
+
+    }
+
+    public void showMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void goMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
 }
